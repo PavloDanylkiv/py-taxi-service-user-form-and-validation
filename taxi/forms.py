25d4222b68c54b38db27375d083baae39cba56b9
Pavlo Danylkiv <danylkiv.pavlo@gmail.com>
2025-04-18 14:22:36 +0100

    improved validation

--- taxi/forms.py
+++ taxi/forms.py
@@ -7,12 +7,17 @@ from taxi.models import Driver, Car
 
 
 def validate_license_number(license_number):
-    if (len(license_number) == 8
-            and license_number[:3].isupper()
-            and license_number[:3].isalpha()
-            and license_number[3:].isdigit()):
-        return license_number
-    raise ValidationError("Please enter existing license number")
+    if len(license_number) != 8:
+        raise ValidationError(
+            "License number must contain 3 letters and 5 digits"
+        )
+    elif not license_number[:3].isdigit() and not license_number[:3].isalpha():
+        raise ValidationError(
+            "License number must contain 3 letters in upper case"
+        )
+    elif not license_number[3:].isdigit():
+        raise ValidationError("License number must contain 5 digits")
+    return license_number
 
 
 class DriverCreateForm(UserCreationForm):
